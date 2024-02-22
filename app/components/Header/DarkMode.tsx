64dcb80fff4f5a94a128eea35f4041c8d97c5d2e
PhucTD <trucka312@gmail.com>
2024-02-22 11:08:37 +0700

    feature fix button theeme

--- app/components/Header/DarkMode.tsx
+++ app/components/Header/DarkMode.tsx
@@ -1,33 +1,56 @@
-'use client';
-import * as React from "react"
-import { useTheme } from "next-themes"
-import { BsMoonStars, BsSun } from "react-icons/bs"
+"use client";
+import * as React from "react";
+import { useTheme } from "next-themes";
+import { BsMoonStars, BsSun } from "react-icons/bs";
 
 interface DarkModeProps {
-    align?: string;
+  align?: string;
 }
 
-const DarkMode = ({ align = 'right' }: DarkModeProps) => {
-    const { setTheme } = useTheme()
-    const [ openDarkMode, setOpenDarkMode ] = React.useState(false)
-    const handleChangeTheme = (theme: string) => {
-        setTheme(theme)
-        setOpenDarkMode(!openDarkMode)
-    }
-    
-    return (
-        <div className="relative inline-block align-middle">
-            <button className="relative border-[1px] p-2 rounded-md overflow-hidden" onClick={() => setOpenDarkMode(!openDarkMode)}>  
-                <BsSun className="h-[1.2rem] w-[1.2rem] transition-all dark:-translate-y-[200%]" />
-                <BsMoonStars className="h-[1.2rem] w-[1.2rem] transition-all absolute translate-y-[200%] top-[100%] dark:top-[50%] dark:-translate-y-[50%]"/>
-            </button>
-            <ul className={`bg-white dark:bg-[#222] text-black dark:text-white absolute top-[120%] ${align}-0 px-5 py-3 rounded-md ${openDarkMode ? 'scale-1' : 'scale-0'} transition-all duration-500 shadow-[0_10px_15px_-3px_rgba(0,0,0,.1),_0_4px_6px_-4px_rgba(0,0,0,.1)]`}>
-                <li className="cursor-pointer" onClick={() => handleChangeTheme("light")}>Light</li>
-                <li className="cursor-pointer" onClick={() => handleChangeTheme("dark")}>Dark</li>
-                <li className="cursor-pointer" onClick={() => handleChangeTheme("system")}>System</li>
-            </ul>
-        </div>
-    )
-}
- 
+const DarkMode = ({ align = "right" }: DarkModeProps) => {
+  const { setTheme } = useTheme();
+  const [pesentTheme, setPesentTheme] = React.useState("system");
+
+  React.useEffect(() => {
+    setTheme("system");
+  }, []);
+  // const [ openDarkMode, setOpenDarkMode ] = React.useState(false)
+  // const handleChangeTheme = (theme: string) => {
+  //     setTheme(theme)
+  //     setOpenDarkMode(!openDarkMode)
+  // }
+
+  return (
+    // <div className="relative inline-block align-middle">
+    //     <button className="relative border-[1px] p-2 rounded-md overflow-hidden" onClick={() => setOpenDarkMode(!openDarkMode)}>
+    //         <BsSun className="h-[1.2rem] w-[1.2rem] transition-all dark:-translate-y-[200%]" />
+    //         <BsMoonStars className="h-[1.2rem] w-[1.2rem] transition-all absolute translate-y-[200%] top-[100%] dark:top-[50%] dark:-translate-y-[50%]"/>
+    //     </button>
+    //     <ul className={`bg-white dark:bg-[#222] text-black dark:text-white absolute top-[120%] ${align}-0 px-5 py-3 rounded-md ${openDarkMode ? 'scale-1' : 'scale-0'} transition-all duration-500 shadow-[0_10px_15px_-3px_rgba(0,0,0,.1),_0_4px_6px_-4px_rgba(0,0,0,.1)]`}>
+    //         <li className="cursor-pointer" onClick={() => handleChangeTheme("light")}>Light</li>
+    //         <li className="cursor-pointer" onClick={() => handleChangeTheme("dark")}>Dark</li>
+    //         <li className="cursor-pointer" onClick={() => handleChangeTheme("system")}>System</li>
+    //     </ul>
+
+    // </div>
+    <div className="">
+      {pesentTheme === "light" ? (
+        <BsMoonStars
+          onClick={() => {
+            setPesentTheme("dark"), setTheme("dark");
+          }}
+          className="h-[1.2rem] w-[1.2rem] "
+        />
+      ) : (
+        <BsSun
+          onClick={() => {
+            setPesentTheme("light"), setTheme("light");
+          }}
+          className="h-[1.2rem] w-[1.2rem] "
+        />
+      )}
+    </div>
+  );
+};
+
 export default DarkMode;
--- app/components/Sections/ContactInformation.tsx
+++ app/components/Sections/ContactInformation.tsx
@@ -1,48 +1,57 @@
-'use client';
+"use client";
 import { FaMapMarkerAlt, FaClock, FaPhoneAlt } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 
-import ContactInformationItem from './ContactInformationItem'
+import ContactInformationItem from "./ContactInformationItem";
 import Social from "../Commons/Social";
 import Heading from "../Commons/Heading";
 
 const ContactInformation = () => {
-    const contactData = [
-        {
-            icon: FaMapMarkerAlt,
-            content: '4th Floor, 75A/59 Hoang Cau Street, Dong Da district, Ha Noi'
-        },
-        {
-            icon: MdEmail,
-            content: 'contact@folinas.com',
-            link: 'mailto: contact@folinas.com'
-        },
-        {
-            icon: FaPhoneAlt,
-            content: '(+84) 096 446 3304',
-            link: 'tel: 096 446 3304'
-        },
-        {
-            icon: FaClock,
-            content: '<p>Mon-Sat 7:30am-4:30pm</p><p>Sun: Close</p>'
-        },
-    ]
-    return (
-        <div>
-            <ul>
-                {contactData?.map((item, index) => {                    
-                    return (
-                        <ContactInformationItem key={index} icon={item.icon} content={item.content} link={item.link&&item.link} />
-                    )
-                })}
-            </ul>
+  const contactData = [
+    {
+      icon: FaMapMarkerAlt,
+      content: "4th Floor, 75A/59 Hoang Cau Street, Dong Da district, Ha Noi",
+    },
+    {
+      icon: MdEmail,
+      content: "contact@folinas.com",
+      link: "mailto: contact@folinas.com",
+    },
+    {
+      icon: FaPhoneAlt,
+      content: "(+84) 096 446 3304",
+      link: "tel: 096 446 3304",
+    },
+    {
+      icon: FaClock,
+      content: "<p>Mon-Sat 7:30am-4:30pm</p><p>Sun: Close</p>",
+    },
+  ];
+  return (
+    <div>
+      <ul>
+        {contactData?.map((item, index) => {
+          return (
+            <ContactInformationItem
+              key={index}
+              icon={item?.icon}
+              content={item?.content}
+              link={item?.link && item?.link}
+            />
+          );
+        })}
+      </ul>
 
-            <div className="mt-10">
-                <Heading heading="Chat with us" headingTag="h2" className="!text-[34px]" />
-                <Social/>
-            </div>
-        </div>
-    )
-}
- 
+      <div className="mt-10">
+        <Heading
+          heading="Connect to us"
+          headingTag="h2"
+          className="!text-[34px]"
+        />
+        <Social />
+      </div>
+    </div>
+  );
+};
+
 export default ContactInformation;
