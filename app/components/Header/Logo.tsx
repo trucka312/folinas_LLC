57981ccc19b825916b55320ae69ad68457290caa
PhucTD <trucka312@gmail.com>
2024-02-22 07:31:12 +0700

    init branch PhucTD

--- app/components/Header/Logo.tsx
+++ app/components/Header/Logo.tsx
@@ -1,33 +1,41 @@
-'use client';
+"use client";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 
 import LogoImage from "@/public/images/Logo.png";
 import Link from "next/link";
+import LoadingBgTheme from "../Loading/LoadingBgTheme/LoadingBgTheme";
 
 const Logo = () => {
-    const pathname = usePathname()
-    const LogoImageRender = () => {
-        return (
-            <Link href='#'>
-                <Image
-                    src={LogoImage}
-                    alt="Logo"
-                    className="cursor-pointer max-h-[52px] w-auto mx-auto"
-                    width={1020}
-                    height={536}
-                    priority={false}
-                />
-            </Link>
-        )
-    }
-    
+  const pathname = usePathname();
+  const LogoImageRender = () => {
     return (
-        <>
-            {pathname === '/' && <h1><LogoImageRender/> </h1> }
-            {pathname !== '/' && <LogoImageRender/> }
-        </>
-    )
-}
+      <Link href="#">
+        <div className="">
+          <LoadingBgTheme />
+          <Image
+            src={LogoImage}
+            alt="Logo"
+            className="cursor-pointer max-h-[52px] w-auto mx-auto"
+            width={1020}
+            height={536}
+            priority={false}
+          />
+        </div>
+      </Link>
+    );
+  };
+
+  return (
+    <>
+      {pathname === "/" && (
+        <h1>
+          <LogoImageRender />{" "}
+        </h1>
+      )}
+      {pathname !== "/" && <LogoImageRender />}
+    </>
+  );
+};
 
 export default Logo;
---
+++ app/components/Loading/LoadingBgTheme/LoadingBgTheme.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React from "react";
+import "./style.css";
+
+const LoadingBgTheme = () => {
+  return (
+    <div>
+      <strong className="loading">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </strong>
+    </div>
+  );
+};
+
+export default LoadingBgTheme;
