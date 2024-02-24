7d04f24365644b7abf4c2d4de2495de4437abf37
PhucTD <trucka312@gmail.com>
2024-02-24 09:06:00 +0700

    PhucTD feature responsive footer

--- app/components/Footer/CopyRight.tsx
+++ app/components/Footer/CopyRight.tsx
@@ -5,18 +5,20 @@ const CopyRight = () => {
   const year = date.getFullYear();
   const router = useRouter();
   return (
-    <div className="border-t-[.5px] my-3 pt-3 mx-auto px-4 flex justify-between">
+    <div className="border-t-[.5px] my-3 pt-3 mx-auto md:px-0 px-4 flex justify-between">
       <p className="text-center">
         © Copyright {year} Folinas LLC. All Rights Reserved.
       </p>
-      <p
-        onClick={() => {
-          router.push("/policy");
-        }}
-        className="cursor-pointer underline"
-      >
-        Privacy Policy
-      </p>
+      <div className="hidden md:block">
+        <p
+          onClick={() => {
+            router.push("/policy");
+          }}
+          className="cursor-pointer underline"
+        >
+          Privacy Policy
+        </p>
+      </div>
     </div>
   );
 };
--- app/components/Footer/FooterLinks.tsx
+++ app/components/Footer/FooterLinks.tsx
@@ -1,29 +1,42 @@
 import Link from "next/link";
 import Heading from "../Commons/Heading";
+import { useRouter } from "next/navigation";
 
 interface FooterLinkItemProps {
-    label: string;
-    href: string;
+  label: string;
+  href: string;
 }
 
 interface FooterLinksProps {
-    items: FooterLinkItemProps[];
-    title?: string;
+  items: FooterLinkItemProps[];
+  title?: string;
 }
 
-const FooterLinks = ({items, title}: FooterLinksProps) => {
-    return (
-        <div className="mt-[29px]">
-            {title && <Heading headingTag="h4" heading={title} />}
-            <ul id="footer-links" className="mt-5">
-                {items.map((item, index) => (
-                    <li key={index} className="mb-2 last:mb-0">
-                        <Link href={item.href}>{item.label}</Link>
-                    </li>
-                )) }
-            </ul>
-        </div>
-    )
-}
- 
+const FooterLinks = ({ items, title }: FooterLinksProps) => {
+  const router = useRouter();
+  return (
+    <div className="mt-[29px]">
+      {title && <Heading headingTag="h4" heading={title} />}
+      <ul id="footer-links" className="mt-5">
+        {items.map((item, index) => (
+          <li key={index} className="mb-2 last:mb-0">
+            <Link href={item.href}>{item.label}</Link>
+          </li>
+        ))}
+      </ul>
+
+      <div className="block md:hidden">
+        <p
+          onClick={() => {
+            router.push("/policy");
+          }}
+          className="cursor-pointer underline"
+        >
+          Privacy Policy
+        </p>
+      </div>
+    </div>
+  );
+};
+
 export default FooterLinks;
